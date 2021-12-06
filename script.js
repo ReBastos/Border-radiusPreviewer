9ec751152cf59aa3fb3293faed7ee73ec147e0b0
Renato <renato.bastos96@gmail.com>
2021-12-05 22:37:44 -0300

    Funcionando(Estado Inicial)

---
+++ script.js
@@ -0,0 +1,12 @@
+function alterarBorder() {
+    let top_left = document.querySelector('#top-left').value;
+    let top_right = document.querySelector('#top-right').value;
+    let bottom_left = document.querySelector('#bottom-left').value;
+    let bottom_right = document.querySelector('#bottom-right').value;
+
+    document.getElementById('radiusBox').style.borderRadius = `${top_left}% ${top_right}% ${bottom_right}% ${bottom_left}%`
+    
+
+    document.getElementById('textGenerated').textContent = `border-radius: ${top_left}% ${top_right}% ${bottom_right}% ${bottom_left}%;`
+
+};
